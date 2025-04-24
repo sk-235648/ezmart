07daeb2e53c930001079a41a719d6f45b25586f1
Satyam117-web <sd4053224@gmail.com>
2025-04-24 12:59:02 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ 
 "use client"
 import Link from 'next/link';
 import { FiFacebook, FiTwitter, FiInstagram, FiYoutube, FiLinkedin } from 'react-icons/fi';
@@ -171,16 +171,4 @@ export default function Footer() {
         </footer>
     );
 }
-=======
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>
-      <h1>This is footer</h1>
-    </div>
-  )
-}
-
-export default Footer
->>>>>>> satyam
+ 
