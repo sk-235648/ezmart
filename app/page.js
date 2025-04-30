54f1920d07b3e5a0e1903a0168be6a2248c460df
Satyam117-web <sd4053224@gmail.com>
2025-04-30 22:20:34 +0530

    folders made

--- app/page.js
+++ app/page.js
@@ -1,23 +1,17 @@
 
 "use client";
 
-import Bigcard from "@/components/Bigcard";
-import CardList from "@/components/Card";
+import Bigcard from "@/components/cards/Bigcard";
+import Frontpg from "@/components/home/Frontpg";
  
-import Frontpg from "@/components/Frontpg";
- 
-
-import Image from "next/image";
-import Card from "@/components/Card";
+import Card from "@/components/cards/Card";
 
  
 
 export default function Home() {
   return (
      
- 
     <>
-     
     <Frontpg/>
 
     <Card/>
