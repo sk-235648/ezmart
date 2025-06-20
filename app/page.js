6df7a6c8522b5ae9dbf055a82c7a0c8a109a9acc
Satyam117-web <sd4053224@gmail.com>
2025-06-20 20:46:36 +0530

    md

--- app/page.js
+++ app/page.js
@@ -5,23 +5,16 @@ import Bigcard from "@/components/cards/Bigcard";
 import Frontpg from "@/components/home/Frontpg";
  
 import Card from "@/components/cards/Card";
-
  
-
 export default function Home() {
   return (
      
     <>
     <Frontpg/>
-
     <Card/>
-    
     <Bigcard/>
-    
   
-
     </>
- 
  
   );
 }
