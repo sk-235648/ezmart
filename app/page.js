14ef8d94a2459428eab76480f60da9410284259e
Satyam117-web <sd4053224@gmail.com>
2025-06-22 12:16:28 +0530

    signup

--- app/page.js
+++ app/page.js
@@ -3,17 +3,15 @@
 
 import Bigcard from "@/components/cards/Bigcard";
 import Frontpg from "@/components/home/Frontpg";
- 
 import Card from "@/components/cards/Card";
  
 export default function Home() {
   return (
      
     <>
-    <Frontpg/>
-    <Card/>
-    <Bigcard/>
-  
+      <Frontpg />
+      <Card />
+      <Bigcard />
     </>
  
   );
